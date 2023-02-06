6b2a87fa82532a773cba5b923e7796b8d0e001d9
Uldana <uldakosh.1606@mail.ru>
2023-02-07 00:58:29 +0600

    Лабораторная работа 3
    В первой задачи я написала программу используя цикл for и while. Во второй написала программу используя функцию range (взяла в первой заданий, так как, я там использовала range ). В трелей задачи использовала рандомайзер и нашла от0 до 125 одну цыфру. В четвертом заданий еще 4 задачи.

--- 1.py
+++ 1.py
@@ -1,13 +1,50 @@
-name= input ("Ваше ФИО:")
-a= int(input ("Вы в каком курсе учитесь?"))
-if a == 1:
-    print ("Вам осталось учиться 3 года")
-elif a==2:
-    print ("Вам осталось учиться 2 года")
-elif a==3:
-    print ("Вам осталось учиться год")
-elif a==4:
-    print ("Вы заканчиваете университет, поздравляю!")
+#Задача 1.1
+a = 0
+while a < 10:
+    print(a)
+    a = a+1
+print('Цикл окончен')
+
+#Задача 1.2
+for x in range(10):
+    print(x)
+print('Цикл окончен')
+
+#Задача 3
+import random
+random_number = random.randint(0, 125)
+print(random_number)
+
+
+#Задача 4.1
+a = int(input())
+b = int(input())
+for i in range(a, b+1):
+    print(i)
+
+#Задача 4.2
+a = int(input())
+b = int(input())
+if a < b:
+    for i in range(a, b + 1):
+        print(i)
 else:
-    print ("Вы не учитесь в университете;)")
-    
+    for i in range(a, b - 1, -1):
+        print(i)
+ 
+#Задача 4.3
+a = int(input())
+b = int(input())
+for i in range(a - (a + 1) % 2, b - b % 2, -2):
+    print(i, end=' ')
+
+#Задача 4.4
+n = int(input())
+sum1 = n
+sum2 = 0
+for i in range(1, n):
+    sum1 += i
+    sum2 += int(input())
+print("Потеряно:", sum1 - sum2)
+
+
