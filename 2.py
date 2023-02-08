e10f146ddd2468de131419df83253027dac4b749
Uldana <uldakosh.1606@mail.ru>
2023-02-09 01:26:51 +0600

    "HeLLo World" должен быть преобразована в "hello world"
    Через string мы расчитываем сколько букв в слове и "HeLLo World" должен быть преобразована в "hello world" или же наоборот

--- 2.py
+++ 2.py
@@ -1,6 +1,12 @@
-a = input() 
-b = input()
-while not(a.isdigit() and b.isdigit()):
-    a = input() 
-    b = input()
-print(int(a) + int(b))
+string = str(input())
+upper = 0
+lower = 0
+for i in string:
+    if i.isupper():
+        upper += 1
+    else:
+        lower += 1
+if upper > lower:
+    print(string.upper())
+else:
+    print(string.lower())
